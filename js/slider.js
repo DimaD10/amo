0b552fc280a59ab304ab37c0b177b1eb03184914
Дима Дзюбенко <dzubenkod7@gmail.com>
2023-07-12 04:27:32 +0300

    editing v.1

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,12 @@
+let marker = true;
+
+document.querySelector('.screen__body').addEventListener('scroll', e => {
+    marker = false
+})
+
 const screenSwiper = new Swiper('.screen-slider', {
     direction: 'vertical',
-    mousewheel: true,
+    mousewheel: marker,
     simulateTouch: false,
     speed: 500,
 });
